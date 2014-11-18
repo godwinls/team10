22a73f2e612fc6acde60c367072c804ec63107dc
godwinls <263209050@qq.com>
2014-11-17 20:43:29 -0800

    change to yisha's version

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
+
 /**
  * Module dependencies.
  */
 
 var express = require('express')
+  , session = require('express-session')
   , routes = require('./routes')
+  , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
-//add by me
-var session= require('express-session')
-  , flash = require('connect-flash');
+  , path = require('path')
+  , home = require('./routes/home');
 
 var app = express();
+
+app.use(session({secret: 'keyboard cat'}));
+app.use(function(req, res, next){ 
+	res.locals.session = req.session; 
+	next(); 
+});
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -20,16 +27,6 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-//add by me
-app.use(flash());
-app.use(express.cookieParser());
-app.use(session({
-  secret: 'ebay',
-  cookie: {maxAge: 86400000},
-  saveUninitialized: true,
-  resave: true
-}));
-
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -38,7 +35,13 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-routes(app);
+//app.get('/', routes.index);
+//app.get('/users', user.list);
+app.get('/signUp', home.signUp);
+app.post('/afterSignUp', home.afterSignUp);
+app.get('/signIn', home.signIn);
+app.get('/afterSignIn', home.afterSignIn);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
