c37a7cad9eec1aebe69833583ca26771a66c4b57
yishafang <yishafang@10.189.26.89>
2014-11-23 12:44:32 -0800

    test app

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 /**
  * Module dependencies.
  */
@@ -5,16 +6,10 @@
 var express = require('express')
   , session = require('express-session')
   , routes = require('./routes')
+  , user = require('./routes/user')
   , http = require('http')
-<<<<<<< HEAD
   , path = require('path')
   , home = require('./routes/home');
-=======
-  , path = require('path');
-//add by me
-var session= require('express-session')
-  , flash = require('connect-flash');
->>>>>>> branch 'master' of https://github.com/godwinls/team10.git
 
 var app = express();
 
@@ -32,16 +27,6 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-//add by me
-app.use(flash());
-app.use(express.cookieParser());
-app.use(session({
-  secret: 'ebay',
-  cookie: {maxAge: 86400000},
-  saveUninitialized: true,
-  resave: true
-}));
-
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -50,7 +35,6 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-<<<<<<< HEAD
 //app.get('/', routes.index);
 //app.get('/users', user.list);
 app.get('/signUp', home.signUp);
@@ -58,9 +42,6 @@ app.post('/afterSignUp', home.afterSignUp);
 app.get('/signIn', home.signIn);
 app.get('/afterSignIn', home.afterSignIn);
 
-=======
-routes(app);
->>>>>>> branch 'master' of https://github.com/godwinls/team10.git
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
