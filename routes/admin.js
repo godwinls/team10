952a99b0dae560772ea3d5bb414eacb0b490bb13
yishafang <yishafang@10.185.214.97>
2014-11-23 18:38:33 -0800

    update signIn_admin

--- routes/admin.js
+++ routes/admin.js
@@ -10,8 +10,17 @@ exports.signIn_admin = function(req, res) {
 }
 
 exports.afterSignIn_admin = function(req, res) {
-	res.render('homepage_admin', {
-		title : 'Ebay'
-	});
-	
+	var adminId = "admin";
+	var adminPass = "ebayadmin";
+	var id = req.param("id");
+	var pass = req.param("pass");
+	if(id == adminId && pass == adminPass) {
+		res.render('homepage_admin', {
+			title : 'Ebay'
+		});
+	}else {
+		res.render('signIn_admin', {
+			title : 'Ebay'
+		});
+	}	
 }
